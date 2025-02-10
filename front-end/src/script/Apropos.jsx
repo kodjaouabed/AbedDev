ac181a385f93f398b5ea812121a96e51e6c4b9e5
kodjaouabed <bossor755@gmail.com>
2025-02-10 23:33:12 +0100

    hddbdhdbdhd

--- front-end/src/script/Apropos.jsx
+++ front-end/src/script/Apropos.jsx
@@ -17,14 +17,12 @@ const Apropos = (props) => {
                
                 >A propos</h5>
                 <p>
-                    <h6>Je suis Abed-Négo Kodjaou, développeur web et mobile spécialisé en React JS et React Native. Passionné par la création d'applications modernes et performantes, j'aide les entreprises à concrétiser leurs idées en solutions numériques efficaces. Mon parcours m'a permis de travailler sur divers projets, dont un système de gestion scolaire présenté lors de ma soutenance, renforçant ainsi mon expertise en développement et gestion de projets complexes.</h6>
+                    Je suis Abed-Négo Kodjaou, développeur web et mobile spécialisé en React JS et React Native. Passionné par la création d'applications modernes et performantes, j'aide les entreprises à concrétiser leurs idées en solutions numériques efficaces. Mon parcours m'a permis de travailler sur divers projets, dont un système de gestion scolaire présenté lors de ma soutenance, renforçant ainsi mon expertise en développement et gestion de projets complexes.
                 </p>
                 <p>
-                    <h6> Je privilégie un code propre et structuré, tout en aimant collaborer avec des équipes multidisciplinaires pour relever des défis techniques. Mon objectif est de concevoir des applications intuitives qui répondent aux besoins des utilisateurs. Toujours ouvert aux nouvelles opportunités, n’hésitez pas à me contacter pour discuter de vos projets web ou mobiles.</h6>
+                    Je privilégie un code propre et structuré, tout en aimant collaborer avec des équipes multidisciplinaires pour relever des défis techniques. Mon objectif est de concevoir des applications intuitives qui répondent aux besoins des utilisateurs. Toujours ouvert aux nouvelles opportunités, n’hésitez pas à me contacter pour discuter de vos projets web ou mobiles.
                 </p>
-                <button
-               
-                >Embauche-moi!</button>
+                <button>Embauche-moi!</button>
             </div>
         </div>
     );
