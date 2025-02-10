26153ecc04f5efe00491e8dc2ca70aa4f85342c0
kodjaouabed <bossor755@gmail.com>
2025-02-10 23:47:15 +0100

    dfhbfhfb

--- front-end/src/script/Apropos.jsx
+++ front-end/src/script/Apropos.jsx
@@ -20,7 +20,7 @@ const Apropos = (props) => {
                     <h6>Je suis Abed-Négo Kodjaou, développeur web et mobile spécialisé en React JS et React Native. Passionné par la création d'applications modernes et performantes, j'aide les entreprises à concrétiser leurs idées en solutions numériques efficaces. Mon parcours m'a permis de travailler sur divers projets, dont un système de gestion scolaire présenté lors de ma soutenance, renforçant ainsi mon expertise en développement et gestion de projets complexes.</h6>
                 </p>
                 <p>
-                    <h6> Je propose maintenance et support pour garantir la sécurité, la stabilité et les mises à jour régulières des sites web et applications mobiles.</h6>
+                    <h6> Je privilégie un code propre et structuré, tout en aimant collaborer avec des équipes multidisciplinaires pour relever des défis techniques. Mon objectif est de concevoir des applications intuitives qui répondent aux besoins des utilisateurs. Toujours ouvert aux nouvelles opportunités, n’hésitez pas à me contacter pour discuter de vos projets web ou mobiles.</h6>
                 </p>
                 <button
                
